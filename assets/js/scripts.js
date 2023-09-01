de076cb4f58b0c7e685a317635f1593071502da3
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-09-01 15:53:35 +0330

    commited

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,7 +18,20 @@ dropdownBtn.forEach((item)=>{
     item.nextElementSibling.classList.toggle('active');
   })
 })
-
+// iran map
+	//fata or justic departments locations
+	var selpro = $("#selectedprovince")[0];
+	$("body").on("mousemove" , ".selectormap .st0", function(event){
+		selpro.style.left = event.clientX - $("#selectedprovince").width()/2 + "px";
+		selpro.style.top = event.clientY - 45 + "px";
+		selpro.innerHTML = $(this).attr("pname");
+	});
+	$("body").on("mouseover" , ".selectormap g", function(event){
+		$("#selectedprovince").addClass("visible");
+	});
+	$("body").on("mouseleave" , ".selectormap g", function(event){
+		$("#selectedprovince").removeClass("visible");
+	});
 // fancybox
 
 Fancybox.bind("[data-fancybox]", {
